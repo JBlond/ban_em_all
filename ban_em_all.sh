d1379905b4e6a21f6c7c4d0a893caeef1602da40
JBlond <leet31337@web.de>
2016-02-11 20:41:47 +0100

    new ban

--- ban_em_all.sh
+++ ban_em_all.sh
@@ -2,6 +2,7 @@ sudo iptables -F
 sudo iptables -N bannedDownloader
 sudo iptables -A bannedDownloader -s 14.120.0.0/16 -j REJECT
 sudo iptables -A bannedDownloader -s 14.121.0.0/16 -j REJECT
+sudo iptables -A bannedDownloader -s 46.39.243.136 -j REJECT
 sudo iptables -A bannedDownloader -s 58.250.0.0/16 -j REJECT
 sudo iptables -A bannedDownloader -s 61.139.0.0/16 -j REJECT
 sudo iptables -A bannedDownloader -s 110.87.89.161/32 -j REJECT
