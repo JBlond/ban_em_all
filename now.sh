5d6f8491316c8e081ced97d3662f32cd8774365b
JBlond <leet31337@web.de>
2016-03-31 16:21:48 +0200

    change

--- now.sh
+++ now.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
- if [ "$#" -eq  "0" ] then
-	echo echo "No arguments supplied"
+if [ "$#" -eq  "0" ] #
+	then
+		echo echo "No arguments supplied"
 else
 	sudo iptables -A INPUT -s $1 -j REJECT
 fi
