277e2e350d9ff27ad06d2bd728891ca7c6ca05f2
JBlond <leet31337@web.de>
2016-04-11 11:36:40 +0200

    DROP it

--- ban_em_all.sh
+++ ban_em_all.sh
@@ -111,7 +111,7 @@ sudo iptables -A bannedDownloader -s 210.36.0.0/16 -j REJECT
 sudo iptables -A bannedDownloader -s 211.103.0.0/12  -j REJECT
 sudo iptables -A bannedDownloader -s 211.136.0.0/16 -j REJECT
 sudo iptables -A bannedDownloader -s 211.138.0.0/12 -j REJECT
-sudo iptables -A bannedDownloader -s 218.0.0.0/8 -j REJECT
+sudo iptables -A bannedDownloader -s 218.0.0.0/8 -j DROP
 sudo iptables -A bannedDownloader -s 219.152.0.0/16 -j REJECT
 sudo iptables -A bannedDownloader -s 219.152.26.210 -j REJECT
 sudo iptables -A bannedDownloader -s 220.160.191.106 -j REJECT
