596504fa8d2072209e49dac81a0535e300370d1e
JBlond <leet31337@web.de>
2016-05-10 12:09:44 +0200

    do not exclude company

--- ban_em_all.sh
+++ ban_em_all.sh
@@ -137,7 +137,6 @@ sudo iptables -A bannedDownloader -s 211.103.0.0/12  -j DROP
 sudo iptables -A bannedDownloader -s 211.128.0.0/12 -j DROP
 sudo iptables -A bannedDownloader -s 211.136.0.0/16 -j REJECT
 sudo iptables -A bannedDownloader -s 211.138.0.0/12 -j REJECT
-sudo iptables -A bannedDownloader -s 213.61.0.0/16 -j REJECT
 sudo iptables -A bannedDownloader -s 218.0.0.0/8 -j DROP
 sudo iptables -A bannedDownloader -s 219.152.0.0/16 -j REJECT
 sudo iptables -A bannedDownloader -s 219.152.26.210 -j REJECT
