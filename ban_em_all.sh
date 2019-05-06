a0481232822d74b39de8bc7dc29a90d69c5cfba1
Mario <JBlond@users.noreply.github.com>
2019-05-06 14:34:17 +0200

    Update ban_em_all.sh

--- ban_em_all.sh
+++ ban_em_all.sh
@@ -88,6 +88,7 @@ sudo iptables -A bannedDownloader -s 61.175.0.0/16 -j DROP
 sudo iptables -A bannedDownloader -s 61.178.0.0/16 -j DROP
 sudo iptables -A bannedDownloader -s 61.216.0.0/16 -j DROP
 sudo iptables -A bannedDownloader -s 84.54.0.0/16 -j DROP #scanner network
+sudo iptables -A bannedDownloader -s 86.104.32.155 -j DROP  # iran scanner
 sudo iptables -A bannedDownloader -s 87.98.163.30 -j DROP #ohv france
 sudo iptables -A bannedDownloader -s 87.247.241.226 -j DROP # UK
 sudo iptables -A bannedDownloader -s 94.249.0.43 -j DROP # JORDAN
@@ -132,6 +133,7 @@ sudo iptables -A bannedDownloader -s 113.120.0.0/16 -j DROP
 sudo iptables -A bannedDownloader -s 113.171.0.0/16 -j DROP # VN Vietnam
 sudo iptables -A bannedDownloader -s 113.222.209.162 -j DROP
 sudo iptables -A bannedDownloader -s 113.250.0.0/16 -j DROP
+sudo iptables -A bannedDownloader -s 114.35.0.0/16 -j DROP #taiwan
 sudo iptables -A bannedDownloader -s 114.80.0.0/16 -j DROP
 sudo iptables -A bannedDownloader -s 114.96.0.0/16 -j DROP
 sudo iptables -A bannedDownloader -s 114.98.0.0/16 -j DROP
