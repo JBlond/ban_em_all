640491413c7df4c6b5c48e75b99d9b821ab35159
Mario <JBlond@users.noreply.github.com>
2019-06-17 09:37:57 +0200

    Update ban_em_all.sh

--- ban_em_all.sh
+++ ban_em_all.sh
@@ -224,6 +224,7 @@ sudo iptables -A bannedDownloader -s 171.5.0.0/16 -j DROP
 sudo iptables -A bannedDownloader -s 171.15.0.0/16 -j DROP
 sudo iptables -A bannedDownloader -s 171.43.0.0/16 -j DROP
 sudo iptables -A bannedDownloader -s 171.223.0.0/16 -j DROP
+sudo iptables -A bannedDownloader -s 173.212.214.137 -j DROP # Exim malware server
 sudo iptables -A bannedDownloader -s 176.9.63.48 -j DROP
 sudo iptables -A bannedDownloader -s 179.223.0.0/16 -j DROP
 sudo iptables -A bannedDownloader -s 180.76.0.0/16 -j DROP
