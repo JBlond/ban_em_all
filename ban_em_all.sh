ba38b718594f10c3d2216f11a5307de67e3be17c
Mario <JBlond@users.noreply.github.com>
2020-04-05 14:08:05 +0200

    Update ban_em_all.sh

--- ban_em_all.sh
+++ ban_em_all.sh
@@ -267,8 +267,8 @@ sudo iptables -A bannedDownloader -s 183.238.0.0/16 -j DROP
 sudo iptables -A bannedDownloader -s 183.240.0.0/16 -j DROP
 sudo iptables -A bannedDownloader -s 183.246.0.0/16 -j DROP
 sudo iptables -A bannedDownloader -s 185.22.187.0/16 -j DROP # attacker SQL injections
-sudo iptables -A bannedDownloader -s 185.157.246.12 j DROP #dns abuser
-sudo iptables -A bannedDownloader -s 185.157.246.123 j DROP #dns abuser
+sudo iptables -A bannedDownloader -s 185.157.246.12 -j DROP #dns abuser
+sudo iptables -A bannedDownloader -s 185.157.246.123 -j DROP #dns abuser
 sudo iptables -A bannedDownloader -s 185.198.58.115 -j DROP #romania scanner
 sudo iptables -A bannedDownloader -s 185.234.0.0/16 -j DROP # scanner / tor ?
 sudo iptables -A bannedDownloader -s 186.194.199.30 -j DROP #BR, RIO scanner
