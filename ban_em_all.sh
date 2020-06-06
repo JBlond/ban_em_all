3277b7c152e02cf38c0ca1cdc6a3d33d5b59cc5c
Mario <JBlond@users.noreply.github.com>
2020-06-06 23:10:10 +0200

    Update ban_em_all.sh

--- ban_em_all.sh
+++ ban_em_all.sh
@@ -339,3 +339,4 @@ sudo iptables -A bannedDownloader -s 223.144.0.0/12 -j DROP
 sudo iptables -A bannedDownloader -s 223.240.0.0/12 -j DROP
 sudo iptables -A INPUT -j bannedDownloader
 sudo iptables -A INPUT -p tcp --dport 3306 -j DROP
+sudo iptables -A INPUT -p tcp --dport 9999 -j DROP
