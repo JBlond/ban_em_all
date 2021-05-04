cea472ebf795b190b81d2a9747b60dab356cedcf
JBlond <leet31337@web.de>
2021-05-04 11:17:51 +0200

    add website attacker

--- blocks/00x.sh
+++ blocks/00x.sh
@@ -12,7 +12,9 @@ sudo iptables -A bannedDownloader -s 1.204.0.0/16 -j DROP
 sudo iptables -A bannedDownloader -s 1.209.171.34 -j DROP # ssh attacker
 sudo iptables -A bannedDownloader -s 1.245.61.144  -j DROP # South Korea
 
+
 sudo iptables -A bannedDownloader -s 2.50.135.168 -j DROP
+sudo iptables -A bannedDownloader -s 2.51.1.61 -j DROP
 
 sudo iptables -A bannedDownloader -s 3.0.115.255 -j DROP
 sudo iptables -A bannedDownloader -s 3.8.12.221 -j DROP
