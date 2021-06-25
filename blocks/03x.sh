52f397dc48d678757cf183e67f9308933d98293f
JBlond <leet31337@web.de>
2021-06-25 16:22:26 +0200

    add 35.234.127.178 cyberscan.io

--- blocks/03x.sh
+++ blocks/03x.sh
@@ -15,6 +15,7 @@ sudo iptables -A bannedDownloader -s 34.251.241.226 -j DROP
 sudo iptables -A bannedDownloader -s 35.183.0.0/12 -j DROP
 sudo iptables -A bannedDownloader -s 35.195.238.142 -j DROP # ssh attacker
 sudo iptables -A bannedDownloader -s 35.168.0.0/16 -j DROP
+sudo iptables -A bannedDownloader -s 35.234.127.178 -j DROP # cyberscan.io
 sudo iptables -A bannedDownloader -s 35.238.252.69 -j DROP # ssh attacker
 
 sudo iptables -A bannedDownloader -s 36.26.83.52 -j DROP # ssh attacker
