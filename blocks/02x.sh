0e79f8bb917503822afa3c3cce24ce940c66de74
JBlond <leet31337@web.de>
2022-02-08 16:05:43 +0100

    add two more

--- blocks/02x.sh
+++ blocks/02x.sh
@@ -2,8 +2,10 @@ echo -e " \e[32mBlock 2x.x\e[0m"
 sudo iptables -A bannedDownloader -s 20.40.82.104 -j DROP
 sudo iptables -A bannedDownloader -s 20.52.134.161 -j DROP # US web site hacker
 sudo iptables -A bannedDownloader -s 20.80.1.21 -j DROP
+sudo iptables -A bannedDownloader -s 20.98.99.122 -j DROP
 sudo iptables -A bannedDownloader -s 20.185.83.210 -j DROP
 sudo iptables -A bannedDownloader -s 20.203.168.56 -j DROP #spammer
+sudo iptables -A bannedDownloader -s 20.206.88.196 -j DROP
 
 sudo iptables -A bannedDownloader -s 23.80.54.0/24 -j DROP
 sudo iptables -A bannedDownloader -s 23.104.141.0/24 -j DROP
