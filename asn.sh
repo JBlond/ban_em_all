40a2d7d734fea4131195a8a2ed436c8a443faa97
JBlond <leet31337@web.de>
2026-06-03 13:08:10 +0200

    update

--- asn.sh
+++ asn.sh
@@ -2,12 +2,12 @@
 
 CHAIN="ASN"
 
-iptables -L $CHAIN -n >/dev/null 2>&1 || iptables -N $CHAIN
+sudo iptables -L $CHAIN -n >/dev/null 2>&1 || sudo iptables -N $CHAIN
 
-iptables -F $CHAIN
+sudo iptables -F $CHAIN
 
 # global logging
-iptables -A $CHAIN \
+sudo iptables -A $CHAIN \
     -m limit --limit 2/min --limit-burst 5 \
     -j LOG --log-prefix "ASN DROP: "
 
@@ -16,10 +16,10 @@ for file in asn/*.txt; do
 
     while read net; do
         [ -z "$net" ] && continue
-        iptables -A $CHAIN -s "$net" -j DROP
+        sudo iptables -A $CHAIN -s "$net" -j DROP
     done < "$file"
 done
 
-iptables -A $CHAIN -j RETURN
+sudo iptables -A $CHAIN -j RETURN
 
-iptables -C INPUT -j $CHAIN 2>/dev/null || iptables -I INPUT 1 -j $CHAIN
+sudo iptables -C INPUT -j $CHAIN 2>/dev/null || sudo iptables -I INPUT 1 -j $CHAIN
--- asn_kill.sh
+++ asn_kill.sh
@@ -4,8 +4,8 @@ CHAIN="ASN"
 
 echo "removing ASN Chain..."
 
-iptables -D INPUT -j $CHAIN 2>/dev/null
-iptables -F $CHAIN 2>/dev/null
-iptables -X $CHAIN 2>/dev/null
+sudo iptables -D INPUT -j $CHAIN 2>/dev/null
+sudo iptables -F $CHAIN 2>/dev/null
+sudo iptables -X $CHAIN 2>/dev/null
 
 echo "ASN deaktivated."
