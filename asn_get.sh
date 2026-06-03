78af1b02af42de242077d011197db5eee16e87cb
JBlond <leet31337@web.de>
2026-06-03 13:06:10 +0200

    update

--- asn_get.sh
+++ asn_get.sh
@@ -11,7 +11,7 @@ LIST=(
 "20473 Vultr"
 "51167 Contabo"
 "63949 Linode-Akamai"
-"9009  M247"
+"9009 M247"
 "12876 Scaleway-Online-net"
 "60781 Leaseweb-NL"
 "28573 Leaseweb-US"
@@ -20,7 +20,7 @@ LIST=(
 "132203 Tencent-Cloud"
 "45102 Alibaba-Cloud"
 "31898 Oracle-Cloud"
-"200373Proxy-Netzwerk"
+"200373 Proxy-Netzwerk"
 )
 
 for entry in "${LIST[@]}"; do
